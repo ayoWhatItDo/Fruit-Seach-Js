2c68c303cafb80a25193ef767f27f24d8253e440
ayoWhatItDo <130956867+ayoWhatItDo@users.noreply.github.com>
2023-05-11 04:15:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const products = ["Apple", "Banana", "Carrot", "Durian", "Eggplant"];
+const products = ["Apple", "Banana", "Carrot", "Durian", "Eggplant", "Oranges", "ayo", "lettuce", "Shrek 2 Dvd"];
 function showSuggestions(str) {
   let productList = document.getElementById("fruitList");
   productList.innerHTML = "";
@@ -6,11 +6,25 @@ function showSuggestions(str) {
   for (let i = 0; i < products.length; i++) {
     if (products[i].toLowerCase().includes(str.toLowerCase())) {
       let product = document.createElement("div");
+      product.classList.add("listItem");
       product.innerHTML = products[i];
       product.addEventListener("click", function() {
-        window.location.href = "product.html/" + products[i];
+        window.location.href = "fruitsearch.html/" + products[i];
       });
       productList.appendChild(product);
     }
   }
 }
+let listItemss = getElementsByClassName("listItems");
+let noResults = true;
+for (i = 0; i < listItems.length; i++) { 
+    if (!listItems[i].innerHTML.toLowerCase().includes(input) || input === "") {
+        listItems[i].style.display="none";
+        continue;
+    }
+    else {
+        listItems[i].style.display="flex";
+        noResults = false; 
+    }
+}
+prooductList.style.display = noResults ? "none" : "block";
